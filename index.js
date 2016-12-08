9ee65a75ee0ef4ded7348b83c9fc41ab3e9504a3
jethro13 <jeanjacques.barbier08@gmail.com>
2016-12-08 20:49:32 +0100

    lkllkm

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
+
+console.log('ok');
+
 let lignesCdes = [];
 let clients = [
   {name : "client1" ,code : "cl1"},
@@ -15,9 +18,28 @@ var init = function (){
     let myTva = tva[idxTva];
     let idxCli=Math.round(Math.random()*5);
     let myCli = clients[idxCli];
-    let montant = Math.random()*1000;
+    let montant = Math.round(Math.random()*1000);
     lignesCdes.push([myCli, myTva, montant]);
 }
   return lignesCdes;
+
+
 }
-console.log(init());
+let tab = [];
+let tab2 = [];
+let max = 0;
+var min = 2000;
+for (let o of init()){
+  tab2 = o;
+  }
+  for (let i = 0;i<tab2.length;i++){
+    if (tab.indexOf(tab2[0].name === -1)) {
+      tab.push(tab2[0]);
+
+    }
+  }
+  console.log(tab);
+
+//const ready = function() {
+//}
+//sdocument.addEventListener("DOMContentLoaded", ready);
